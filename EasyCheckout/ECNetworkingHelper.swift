89a2f20077f850fb79a51dee39231d3f90c93c21
parry <parry@parrys-air.attlocal.net>
2016-08-03 21:51:19 -0700

    getting started

---
+++ EasyCheckout/ECNetworkingHelper.swift
@@ -0,0 +1,30 @@
+//
+//  ECNetworkingHelper.swift
+//  EasyCheckout
+//
+//  Created by parry on 8/3/16.
+//  Copyright © 2016 MCP. All rights reserved.
+//
+
+import Foundation
+
+
+final class ECNetworkingHelper: NSObject {
+    static let sharedInstance = ECNetworkingHelper()
+
+func fetchCurrentFix(completionHandler: (data: [ECItem], error: NSError?) -> Void) -> Void {
+
+}
+
+
+func updateCurrentFix(keptItemsArray: [String], completionHandler: (data: [WMWeatherResponseObject], error: NSError?) -> Void) -> Void {
+
+}
+
+
+
+}
+
+
+
+
---
+++ EasyCheckout/ECSelectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ECSelectionViewController.swift
+//  EasyCheckout
+//
+//  Created by parry on 8/3/16.
+//  Copyright © 2016 MCP. All rights reserved.
+//
+
+import Foundation
