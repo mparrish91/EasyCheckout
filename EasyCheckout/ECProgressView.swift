f4533c9ed5eb3611d0df81faced49c687a73406c
parry <parry@parrys-air.attlocal.net>
2016-08-06 23:43:32 -0700

    wrote Progress View

--- EasyCheckout/ECProgressView.swift
+++ EasyCheckout/ECProgressView.swift
@@ -18,7 +18,7 @@ final class ECProgressView: UIView {
 
     override init(frame: CGRect) {
         self.count = Int()
-        self.coloredUpToIndex = Int()
+        self.coloredUpToIndex = 0
         self.layerArray = [CAShapeLayer]()
         super.init(frame: frame)
     }
@@ -37,14 +37,37 @@ final class ECProgressView: UIView {
     func createShapeLayer(count: Int) {
 
         //calculate width of the view
+        let width = self.frame.width
 
         //divide by the number of items(count) = single progress length
+        let singleProgressLength = width/CGFloat(count)
+
 
         // take 10% off that as the gap length
+        let gapLength = singleProgressLength * 0.10
 
         //new single progress length = progress length - gap
+        let newSingleProgressLength = singleProgressLength - gapLength
 
         //loop through the count
+        for rect in 0...count {
+            let layer = CAShapeLayer()
+            layer.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: newSingleProgressLength, height: self.frame.height), cornerRadius: 50).CGPath
+
+            //start at 0 for the first layer
+            if count == 0 {
+                layer.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: newSingleProgressLength, height: self.frame.height), cornerRadius: 50).CGPath
+
+            }else {
+                layer.path = UIBezierPath(roundedRect: CGRect(x: 0 + (newSingleProgressLength + gapLength), y: 0, width: newSingleProgressLength, height: self.frame.height), cornerRadius: 50).CGPath
+
+            }
+
+            layer.fillColor = UIColor.grayColor().CGColor
+            self.layer.addSublayer(layer)
+
+            layerArray.append(rect)
+        }
 
         // for each draw a rectangle cashape layer
         //(keep incrementing by the x by the width + gap)
@@ -59,8 +82,20 @@ final class ECProgressView: UIView {
     func updateView() {
 
         //select next layer in array
+        let layer = layerArray[coloredUpToIndex]
+
 
         //change color of the layer in animation
+
+        UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+
+            layer.fillColor = UIColor.yellowColor().CGColor
+
+
+            }, completion: { (finished: Bool) -> Void in
+        })
+
+        coloredUpToIndex++
 
 
     }
