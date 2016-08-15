ed85986ee85dc8ad6a4ace12cfc3919e66645002
parry <parry@parrys-air.attlocal.net>
2016-08-14 23:36:46 -0700

    finishing tests

--- EasyCheckoutUITests/EasyCheckoutUITests.swift
+++ EasyCheckoutUITests/EasyCheckoutUITests.swift
@@ -13,24 +13,22 @@ class EasyCheckoutUITests: XCTestCase {
 
     let app = XCUIApplication()
 
-
-
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        app.launch()
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+
+        // Before each specific test
+
+
     }
 
     func testSelectionVCScreenOne() {
 
+        app.launch()
 
-        let app = XCUIApplication()
+        //Wait for app to download data
+        sleep(1)
 
         //Does Nav Bar exist
         XCTAssert(app.navigationBars["My Items"].exists)
@@ -60,21 +58,22 @@ class EasyCheckoutUITests: XCTestCase {
 
         let myCartLabel = app.staticTexts["My Cart"]
         let progressView = element.childrenMatchingType(.Other).elementBoundByIndex(1)
-        let nameLabel = app.staticTexts["Dorothy Layered Hammered Cuff"]
         let nameIcon = app.images["nameIcon"]
         let brandIcon = app.images["brandIcon"]
         let costIcon = app.images["costIcon"]
+        let nameLabel = app.staticTexts["Dorothy Layered Hammered Cuff"]
         let brandLabel = app.staticTexts["ZAD"]
         let costLabel = app.staticTexts["34.00"]
 
+        XCTAssert(nameLabel.exists)
+        XCTAssert(brandLabel.exists)
+        XCTAssert(costLabel.exists)
+
         XCTAssert(myCartLabel.exists)
         XCTAssert(progressView.exists)
-        XCTAssert(nameLabel.exists)
         XCTAssert(nameIcon.exists)
         XCTAssert(brandIcon.exists)
         XCTAssert(costIcon.exists)
-        XCTAssert(brandLabel.exists)
-        XCTAssert(costLabel.exists)
 
         //Keep Button exist? Is user notified?
         let keepButton = app.buttons["Keep :)"]
@@ -88,117 +87,333 @@ class EasyCheckoutUITests: XCTestCase {
         let okButton = app.alerts.collectionViews.buttons["OK"]
         okButton.tap()
 
+        //Load next itemVC
         nextButton.tap()
 
     }
 
 
+    func testSelectionVCScreenTwo() {
 
-    func recordingTestExample() {
+        app.launch()
+
+        //Wait for app to download data
+        sleep(1)
+
+        //Does Nav Bar exist
+        XCTAssert(app.navigationBars["My Items"].exists)
+
+        //Does Next Button exist
+        let nextButton = XCUIApplication().navigationBars["My Items"].buttons["next"]
+        XCTAssert(nextButton.exists)
+
+        //Does CollectionView Exist
+        let element = app.otherElements.containingType(.NavigationBar, identifier:"My Items").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        let collectionView = element.childrenMatchingType(.CollectionView).element
+        XCTAssert(collectionView.exists)
+
+        //Does collectionview image exist
+        let image = app.collectionViews.cells.otherElements.childrenMatchingType(.Image).element
+        XCTAssert(image.exists)
+
+        //Scroll Collectionview?
+        collectionView.tap()
+
+        //Does Page Control exist?
+        let pageControl = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        XCTAssert(pageControl.exists)
 
 
-        
-        let nextButton = myItemsNavigationBar.buttons["next"]
-        nextButton.tap()
-        image.tap()
-        app.staticTexts["Black and Gold Dangle Necklace"].tap()
-        app.staticTexts["Adia Kibur"].tap()
-        app.staticTexts["50.00"].tap()
+        //Do Labels and ImageViews Exit
+
+        let myCartLabel = app.staticTexts["My Cart"]
+        let progressView = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        let nameIcon = app.images["nameIcon"]
+        let brandIcon = app.images["brandIcon"]
+        let costIcon = app.images["costIcon"]
+        let nameLabel = app.staticTexts["Black and Gold Dangle Necklace"]
+        let brandLabel = app.staticTexts["Adia Kibur"]
+        let costLabel = app.staticTexts["50.00"]
+
+        XCTAssert(nameLabel.exists)
+        XCTAssert(brandLabel.exists)
+        XCTAssert(costLabel.exists)
+
+        XCTAssert(myCartLabel.exists)
+        XCTAssert(progressView.exists)
+        XCTAssert(nameIcon.exists)
+        XCTAssert(brandIcon.exists)
+        XCTAssert(costIcon.exists)
+
+        //Keep Button exist? Is user notified?
+        let keepButton = app.buttons["Keep :)"]
+        XCTAssert(keepButton.exists)
         keepButton.tap()
-        okButton.tap()
-        nextButton.tap()
-        app.staticTexts["Henry Birds on Branch Infinity Scarf"].tap()
-        app.staticTexts["Octavia"].tap()
-        app.staticTexts["28.00"].tap()
-        nextButton.tap()
-        app.staticTexts["Corinna Striped Dolman Top"].tap()
-        app.staticTexts["Market & Spruce"].tap()
-        app.staticTexts["48.00"].tap()
-        image.tap()
-        image.tap()
-        nextButton.tap()
-        image.tap()
-        image.tap()
-        app.staticTexts["Anita Skinny Pant"].tap()
-        app.staticTexts["Liverpool"].tap()
-        app.staticTexts["78.00"].tap()
-        collectionView.tap()
-        collectionView.tap()
-        collectionView.tap()
-        collectionView.tap()
-        element.tap()
-        element.tap()
-        element.tap()
-        element.tap()
-        myItemsNavigationBar.buttons["done"].tap()
-        
-        let tablesQuery = app.tables
-        tablesQuery.staticTexts["Dorothy Layered Hammered Cuff"].tap()
-        tablesQuery.staticTexts["Black and Gold Dangle Necklace"].tap()
 
-        /////
+        //Alert View??
+        let alertView = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Alert).element
+        XCTAssert(alertView.exists)
+
+        let okButton = app.alerts.collectionViews.buttons["OK"]
+        okButton.tap()
         
-        let app = XCUIApplication()
-        let myItemsNavigationBar = app.navigationBars["My Items"]
-        let nextButton = myItemsNavigationBar.buttons["next"]
+        //Load next itemVC
         nextButton.tap()
-        nextButton.tap()
-        app.buttons["Keep :)"].tap()
-        app.alerts.collectionViews.buttons["OK"].tap()
-        nextButton.tap()
-        nextButton.tap()
-        myItemsNavigationBar.buttons["done"].tap()
+
+    }
+
+    func testSelectionVCScreenThree() {
+
+        app.launch()
+
+        //Wait for app to download data
+        sleep(1)
+
+        //Does Nav Bar exist
+        XCTAssert(app.navigationBars["My Items"].exists)
+
+        //Does Next Button exist
+        let nextButton = XCUIApplication().navigationBars["My Items"].buttons["next"]
+        XCTAssert(nextButton.exists)
+
+        //Does CollectionView Exist
+        let element = app.otherElements.containingType(.NavigationBar, identifier:"My Items").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        let collectionView = element.childrenMatchingType(.CollectionView).element
+        XCTAssert(collectionView.exists)
+
+        //Does collectionview image exist
+        let image = app.collectionViews.cells.otherElements.childrenMatchingType(.Image).element
+        XCTAssert(image.exists)
+
+        //Scroll Collectionview?
+        collectionView.tap()
+
+        //Does Page Control exist?
+        let pageControl = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        XCTAssert(pageControl.exists)
+
+
+        //Do Labels and ImageViews Exit
+
+        let myCartLabel = app.staticTexts["My Cart"]
+        let progressView = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        let nameIcon = app.images["nameIcon"]
+        let brandIcon = app.images["brandIcon"]
+        let costIcon = app.images["costIcon"]
+        let nameLabel = app.staticTexts["Henry Birds on Branch Infinity Scarf"]
+        let brandLabel = app.staticTexts["Octavia"]
+        let costLabel = app.staticTexts["28.00"]
+
+        XCTAssert(nameLabel.exists)
+        XCTAssert(brandLabel.exists)
+        XCTAssert(costLabel.exists)
+
+        XCTAssert(myCartLabel.exists)
+        XCTAssert(progressView.exists)
+        XCTAssert(nameIcon.exists)
+        XCTAssert(brandIcon.exists)
+        XCTAssert(costIcon.exists)
+
+        //Keep Button exist? Is user notified?
+        let keepButton = app.buttons["Keep :)"]
+        XCTAssert(keepButton.exists)
+        keepButton.tap()
+
+        //Alert View??
+        let alertView = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Alert).element
+        XCTAssert(alertView.exists)
+
+        let okButton = app.alerts.collectionViews.buttons["OK"]
+        okButton.tap()
         
+        //Load next itemVC
+        nextButton.tap()
+        
+    }
+
+    func testSelectionVCScreenFour() {
+
+        app.launch()
+
+        //Wait for app to download data
+        sleep(1)
+
+        //Does Nav Bar exist
+        XCTAssert(app.navigationBars["My Items"].exists)
+
+        //Does Next Button exist
+        let nextButton = XCUIApplication().navigationBars["My Items"].buttons["next"]
+        XCTAssert(nextButton.exists)
+
+        //Does CollectionView Exist
+        let element = app.otherElements.containingType(.NavigationBar, identifier:"My Items").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        let collectionView = element.childrenMatchingType(.CollectionView).element
+        XCTAssert(collectionView.exists)
+
+        //Does collectionview image exist
+        let image = app.collectionViews.cells.otherElements.childrenMatchingType(.Image).element
+        XCTAssert(image.exists)
+
+        //Scroll Collectionview?
+        collectionView.tap()
+
+        //Does Page Control exist?
+        let pageControl = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        XCTAssert(pageControl.exists)
+
+
+        //Do Labels and ImageViews Exit
+
+        let myCartLabel = app.staticTexts["My Cart"]
+        let progressView = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        let nameIcon = app.images["nameIcon"]
+        let brandIcon = app.images["brandIcon"]
+        let costIcon = app.images["costIcon"]
+        let nameLabel = app.staticTexts["Corinna Striped Dolman Top"]
+        let brandLabel = app.staticTexts["Market & Spruce"]
+        let costLabel = app.staticTexts["48.00"]
+
+        XCTAssert(nameLabel.exists)
+        XCTAssert(brandLabel.exists)
+        XCTAssert(costLabel.exists)
+
+        XCTAssert(myCartLabel.exists)
+        XCTAssert(progressView.exists)
+        XCTAssert(nameIcon.exists)
+        XCTAssert(brandIcon.exists)
+        XCTAssert(costIcon.exists)
+
+        //Keep Button exist? Is user notified?
+        let keepButton = app.buttons["Keep :)"]
+        XCTAssert(keepButton.exists)
+        keepButton.tap()
+
+        //Alert View??
+        let alertView = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Alert).element
+        XCTAssert(alertView.exists)
+
+        let okButton = app.alerts.collectionViews.buttons["OK"]
+        okButton.tap()
+        
+        //Load next itemVC
+        nextButton.tap()
+        
+    }
+
+    func testSelectionVCScreenFive() {
+
+        app.launch()
+
+        //Wait for app to download data
+        sleep(1)
+
+        //Does Nav Bar exist
+        XCTAssert(app.navigationBars["My Items"].exists)
+
+        //Does Next Button exist
+        let doneButton = XCUIApplication().navigationBars["My Items"].buttons["done"]
+        XCTAssert(doneButton.exists)
+
+        //Does CollectionView Exist
+        let element = app.otherElements.containingType(.NavigationBar, identifier:"My Items").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        let collectionView = element.childrenMatchingType(.CollectionView).element
+        XCTAssert(collectionView.exists)
+
+        //Does collectionview image exist
+        let image = app.collectionViews.cells.otherElements.childrenMatchingType(.Image).element
+        XCTAssert(image.exists)
+
+        //Scroll Collectionview?
+        collectionView.tap()
+
+        //Does Page Control exist?
+        let pageControl = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        XCTAssert(pageControl.exists)
+
+
+        //Do Labels and ImageViews Exit
+
+        let myCartLabel = app.staticTexts["My Cart"]
+        let progressView = element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        let nameIcon = app.images["nameIcon"]
+        let brandIcon = app.images["brandIcon"]
+        let costIcon = app.images["costIcon"]
+        let nameLabel = app.staticTexts["Anita Skinny Pant"]
+        let brandLabel = app.staticTexts["Liverpool"]
+        let costLabel = app.staticTexts["78.00"]
+
+        XCTAssert(nameLabel.exists)
+        XCTAssert(brandLabel.exists)
+        XCTAssert(costLabel.exists)
+
+        XCTAssert(myCartLabel.exists)
+        XCTAssert(progressView.exists)
+        XCTAssert(nameIcon.exists)
+        XCTAssert(brandIcon.exists)
+        XCTAssert(costIcon.exists)
+
+        //Keep Button exist? Is user notified?
+        let keepButton = app.buttons["Keep :)"]
+        XCTAssert(keepButton.exists)
+        keepButton.tap()
+
+        //Alert View??
+        let alertView = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Alert).element
+        XCTAssert(alertView.exists)
+
+        let okButton = app.alerts.collectionViews.buttons["OK"]
+        okButton.tap()
+        
+        //Load next itemVC
+        doneButton.tap()
+
+    }
+
+
+
+    func testInvoiceVC() {
+
+        //Does Nav Bar exist
+        let navTitle = app.navigationBars["Checkout"].staticTexts["Checkout"]
+        XCTAssert(navTitle.exists)
+
+        //Does Table exist
+        let tablesQuery = app.tables
+        let firstCell = tablesQuery.staticTexts["Dorothy Layered Hammered Cuff"]
+        let second = tablesQuery.staticTexts["Black and Gold Dangle Necklace"]
+        let third = tablesQuery.staticTexts["Henry Birds on Branch Infinity Scarf"]
+        let fourth = tablesQuery.staticTexts["Corinna Striped Dolman Top"]
+        let fifth = tablesQuery.staticTexts["Anita Skinny Pant"]
+
+        XCTAssert(firstCell.exists)
+        XCTAssert(second.exists)
+        XCTAssert(third.exists)
+        XCTAssert(fourth.exists)
+        XCTAssert(fifth .exists)
+
+
+
+        //Do Labels and values exist
+        app.staticTexts["Subtotal"].tap()
+        app.staticTexts["Tax"].tap()
+        app.staticTexts["Total"].tap()
+        app.staticTexts["238.00"].tap()
+        app.staticTexts["23.00"].tap()
+        app.staticTexts["261.00"].tap()
+        
+        let line = app.otherElements.containingType(.NavigationBar, identifier:"Checkout").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        XCTAssert(line.exists)
+
+        //Does confirm button exist
+        let confirm = app.buttons["Confirm"]
+        XCTAssert(confirm.exists)
+
+
 
     }
 
 
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
 
-        sleep(1)
-
-
-        let button = app.buttons["Keep :)"]
-        XCTAssert(button.exists)
-
-
-
-        sleep(1)
-
-//        //verify title
-//        XCTAssert(app.navigationBars["My Items"].exists)
-//
-////        sleep(1)
-////
-//
-//        XCTAssert(app.staticTexts["My Cart"].exists)
-        XCTAssert(app.buttons["Keep :)"].exists)
-
-//        app.buttons["Keep :)"].tap()
-//
-//        //push keep button
-//        app.alerts[""].buttons["Keep :)"].tap()
-//
-//        //assert the item has text and image have changed
-//        XCTAssert(app.navigationBars["Volleyball?"].exists)
-//
-//
-//
-//
-//
-//        //verify title
-//        XCTAssert(app.navigationBars["Checkout"].exists)
-//
-//        XCTAssert(app.staticTexts["Subtotal"].exists)
-//        XCTAssert(app.staticTexts["Tax"].exists)
-//        XCTAssert(app.staticTexts["Total"].exists)
-//        XCTAssert(app.staticTexts["Confirm"].exists)
-//
-
-
-    }
     
 }
